2dfcdf34107a7c63d8970eea22bdf9b6c89d8618
Mamun1646 <mimbd01130004@gmail.com>
2022-11-27 14:30:50 +0600

    cor policy allow

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,7 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     console.log(process.env.port);
+    app.enableCors();
     await app.listen(4005);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(process.env.port)
+  app.enableCors();
   await app.listen(4005);
 }
 bootstrap();
